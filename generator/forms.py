59a98257b5c3b54883b67caf2626085fc9f66126
stevetwalker <29964517+stevetwalker@users.noreply.github.com>
2020-07-18 16:51:48 -0700

    Initial Flask form fields for generator

---
+++ generator/forms.py
@@ -0,0 +1,14 @@
+"""Forms to securely accept equation parameters."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+
+class EquationForm(FlaskForm):
+    equation = StringField("Equation: ", validators=[DataRequired()])
+    submit = SubmitField("Proceed")
+
+class VariableForm(FlaskForm):
+    minimum = IntegerField("Min", validators=[DataRequired()])
+    maximum = IntegerField("Max", validators=[DataRequired()])
+    zero_ok = BooleanField("Zero Ok", default=True)
